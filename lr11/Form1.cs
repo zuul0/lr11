8ccae1230d7997a8e295101313eb831c7209b7cc
Zukhr <Zukhr@DESKTOP-9280BU9>
2023-04-11 21:16:31 +0300

    Начало работы

--- lr11/Form1.cs
+++ lr11/Form1.cs
@@ -16,5 +16,10 @@ namespace lr11
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ lr11/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr11
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base("DbConnection") { }
+        public DbSet<User> Users { get; set; }
+        static UserContext()
+        {
+            Database.SetInitializer<UserContext>(new DropCreateDatabaseAlways<UserContext>());
+        }
+    }
+}
